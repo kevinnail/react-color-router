e56a1f1815259cfee2d4e090da814c2d4f54ee9d
kevinnail <fumalicious@gmail.com>
2023-01-24 10:55:13 -0800

    added route handling, links to header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
-import Main from './components/Colors/Colors.js';
+import RGB from './components/RBG/RGB.js';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-    </div>
+    <main className="App">
+      <Switch>
+        <Route path="/RGB/:R/:G/:B" component={Header} />
+      </Switch>
+
+      <RGB />
+    </main>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 export default function Header() {
   return (
     <ul className="header">
-      <li>Link 1</li>
-      <li>Link 2</li>
-      <li>Link 3</li>
+      <li>
+        <NavLink to={`/RGB/123/234/142`}>123/234/142</NavLink>
+      </li>
+      <li>
+        {' '}
+        <NavLink to={`/RGB/231/034/022`}>231/034/022</NavLink>
+      </li>
+      <li>
+        {' '}
+        <NavLink to={`/RGB/005/001/452`}>005/001/452</NavLink>
+      </li>
     </ul>
   );
 }
