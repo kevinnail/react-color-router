01af54ae696c7bcb57fef80f489f81c79741704a
kevinnail <fumalicious@gmail.com>
2023-01-23 19:06:03 -0800

    Header and Main files added with skeleton code

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+export default function Header() {
+  return (
+    <ul className="header">
+      <li>Link 1</li>
+      <li>Link 2</li>
+      <li>Link 3</li>
+    </ul>
+  );
+}
